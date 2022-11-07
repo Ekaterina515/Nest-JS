bffd4259a8ec8dd7f8e46a10d8eef7df166dbeaf
EkaterinaF <9150221@mail.ru>
2022-11-07 16:15:13 +0200

    dz3

--- src/api/modules/comments/comments.module.ts
+++ src/api/modules/comments/comments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { CommentsController } from '../../controllers/comments.controller';
+import { PostsController } from '../../controllers/posts.controller';
+import { LoggerModule } from '../logger/logger.module';
 import { PostsModule } from '../posts/posts.module';
 import { CommentsService } from './comments.service';
 
 @Module({
-  imports: [PostsModule],
-  controllers: [CommentsController],
+  imports: [PostsModule, LoggerModule],
+  controllers: [PostsController],
   providers: [Array, CommentsService],
   exports: [CommentsService],
 })
-export class CommentsModule { }
+export class CommentsModule {}
